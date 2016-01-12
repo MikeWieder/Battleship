a36a1b0749fc4c1ae28533c88b28f9f8eb3ad96a
Shuttle44 <ludger.rothermel@gmail.com>
2016-01-12 13:48:09 +0100

    Final

--- Code.java
+++ Code.java
@@ -7,17 +7,10 @@ public static void main (String[] args)
 {
 int a = 2;
 int b = 0;
-float c;
 
 if (a<=b)
 {
-<<<<<<< .mine
-int z = b / a;
-||||||| .r8
 int x = b - a;
-=======
-int x = b - a +5;
->>>>>>> .r9
 }
 
 }
