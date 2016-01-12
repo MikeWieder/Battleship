73b8fb1f806617c22e3daf97db0a1365db221bf8
Shuttle44 <ludger.rothermel@gmail.com>
2016-01-12 13:46:41 +0100

    Conflicted

--- Code.java
+++ Code.java
@@ -7,10 +7,17 @@ public static void main (String[] args)
 {
 int a = 2;
 int b = 0;
+float c;
 
 if (a<=b)
 {
+<<<<<<< .mine
+int z = b / a;
+||||||| .r8
+int x = b - a;
+=======
 int x = b - a +5;
+>>>>>>> .r9
 }
 
 }
